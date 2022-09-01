ace6c3b01f59e46c52452f4a3ea6696df455217e
Pratham Paliwal <prathampaliwal@Prathams-MacBook-Pro.local>
2022-08-31 20:55:51 -0700

    add Barebone application

---
+++ src/main/java/com/ubc/textanalyzer/TextanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ubc.textanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubc/textanalyzer/TextanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubc.textanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
